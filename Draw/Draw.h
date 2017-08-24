32513eed4f2519a96b6ce03f863b7352c5182c93
Roman Savrulin <romansavrulin@gmail.com>
2017-08-24 13:01:41 +0300

    add extern C definitions to draw layer

--- Draw/Draw.h
+++ Draw/Draw.h
@@ -5,16 +5,23 @@
 #include <stdbool.h>
 #include "Fonts/Font.h"
 
-typedef struct {
-	void(*vFramebufferRectangle)(uint16_t usX0, uint16_t usY0, uint16_t usX1, uint16_t usY1, uint16_t usColor, bool bFill);
-	void(*vFramebufferPutChar)(uint16_t usX, uint16_t usY, char ASCI, xFont pubFont, uint16_t usColor, uint16_t usBackground, bool bFillBg);
-	void(*vFramebufferHLine)(uint16_t usX0, uint16_t usY0, uint16_t usY1, uint16_t usColor);
-	void(*vFramebufferVLine)(uint16_t usX0, uint16_t usY0, uint16_t usX1, uint16_t usColor);
-	void(*bFramebufferPicture)(int16_t sX0, int16_t sY0, unsigned short const* pusPicture);
-}LCD_Glue;
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
 
-void vWidgetSetLCD(LCD_Glue * LCD);
-LCD_Glue * pxWidgetGetLCD();
+	typedef struct {
+		void(*vFramebufferRectangle)(uint16_t usX0, uint16_t usY0, uint16_t usX1, uint16_t usY1, uint16_t usColor, bool bFill);
+		void(*vFramebufferPutChar)(uint16_t usX, uint16_t usY, char ASCI, xFont pubFont, uint16_t usColor, uint16_t usBackground, bool bFillBg);
+		void(*vFramebufferHLine)(uint16_t usX0, uint16_t usY0, uint16_t usY1, uint16_t usColor);
+		void(*vFramebufferVLine)(uint16_t usX0, uint16_t usY0, uint16_t usX1, uint16_t usColor);
+		void(*bFramebufferPicture)(int16_t sX0, int16_t sY0, unsigned short const* pusPicture);
+	}LCD_Glue;
 
+	void vWidgetSetLCD(LCD_Glue * LCD);
+	LCD_Glue * pxWidgetGetLCD();
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
 
 #endif // !EMGUI_DRAW_H
