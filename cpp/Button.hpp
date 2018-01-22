1e25806b36a35da0df280dc3e78054a56adbc3be
Roman Savrulin <savrulin@flexibity.com>
2018-01-22 20:36:11 +0300

    test implementation of button toggle on cpp bindings

--- cpp/Button.hpp
+++ cpp/Button.hpp
@@ -24,12 +24,54 @@ namespace emGUI {
 			vButtonSetOnClickHandler(xThis, [](xWidget *pxW) -> bool {
 				if (auto o = getObject(pxW)) {
 					if(o->onClick)
-						o->onClick();
+						o->onClick(o);
 				}
 				return true;
 			});
 		}
 
-		std::function<void()> onClick;
+		std::function<void(Button *b)> onClick;
+	};
+
+	class ButtonToggle : public Button {
+	public:
+		ButtonToggle():_state(false) {};
+
+		ButtonToggle(uint16_t X, uint16_t Y, uint16_t W, uint16_t H, const char *text, Widget * parent):
+			Button(X, Y, W, H, text, parent) {
+			
+			vButtonSetOnClickHandler(xThis, [](xWidget *pxW) -> bool {
+				if (auto o = (ButtonToggle *) getObject(pxW)) {
+					o->click();
+				}
+				return true;
+			});
+			toggleDecorator();
+		}
+
+		void state(bool state) {
+			_state = state;
+			toggleDecorator();
+		}
+
+		void click() {
+			if (onClick)
+				onClick(this);
+			_state = !_state;
+			toggleDecorator();
+			if (onToggle)
+				onToggle(this, _state);
+		};
+
+		virtual void toggleDecorator() {
+			if (_state)
+				vWidgetSetBgColor(get(), EMGUI_COLOR_RED, false);
+			else
+				vWidgetSetBgColor(get(), EMGUI_COLOR_GREEN, false);
+		}
+
+		std::function<void(ButtonToggle *b, bool)> onToggle;
+	protected:
+		bool _state;
 	};
 }
