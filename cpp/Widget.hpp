863b1946af16286265a0a88d0266b15d01acb256
Roman Savrulin <savrulin@flexibity.com>
2018-01-23 11:25:03 +0300

    add make_shared for silly gcc compiler

--- cpp/Widget.hpp
+++ cpp/Widget.hpp
@@ -43,3 +43,17 @@ namespace emGUI {
 		}
 	};
 }
+
+#if __cplusplus <= 201103L && !defined(_MSC_VER)
+
+namespace std {
+
+  template<typename T, typename... Args>
+  std::unique_ptr<T> make_unique(Args&&... args)
+  {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+  }
+
+}
+
+#endif
